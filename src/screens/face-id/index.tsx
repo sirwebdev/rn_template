b60e629aa58170ee950288c971e9d2a99195dad9
pmartinsdev <martins20.tech@gmail.com>
2024-04-18 22:29:27 -0300

    feat: add faceID into project

--- src/screens/face-id/index.tsx
+++ src/screens/face-id/index.tsx
@@ -1,10 +1,11 @@
 import { useCallback } from "react";
-import { View } from "react-native";
-import { CaretLeft } from "phosphor-react-native";
+import { Alert, View } from "react-native";
+import { CaretLeft, Fingerprint } from "phosphor-react-native";
 import { NavigationProp, ParamListBase } from "@react-navigation/native";
+import ReactNativeBiometrics, { BiometryTypes } from 'react-native-biometrics';
 
 import { SafeArea } from "../../styles/safe-area"
-import { FaceIDScreenView, HeaderButton, primaryColor } from "./style"
+import { ButtonText, FaceIDScreenView, FaceIdButton, HeaderButton, SectionView, SmallText, TitleContainer, TitleText, primaryColor } from "./style"
 
 interface FaceIDScreenProps {
   navigation: NavigationProp<ParamListBase>
@@ -15,6 +16,81 @@ export const FaceIDScreen = ({ navigation }: FaceIDScreenProps) => {
     navigation.goBack()
   }, [])
 
+  const askPermissionForFaceID = useCallback((available: boolean, biometryType: string, saveBiometryKeyCallback?: () => void | Promise<void>): void => {
+    if (available && biometryType === BiometryTypes.FaceID) {
+      Alert.alert(
+        'Face ID',
+        'Would you like to enable Face ID authentication for the next time?',
+        [
+          {
+            text: 'Yes please',
+            onPress: saveBiometryKeyCallback
+          },
+          { text: 'Cancel', style: 'cancel' },
+        ],
+      );
+    }
+  }, [])
+
+  const handleAuthenticateWithFaceID = useCallback(async (userID: string, rnBiometricsInstance: ReactNativeBiometrics) => {
+    const timestamp = Math.round(
+      new Date().getTime() / 1000,
+    ).toString();
+    const payload = `${userID}__${timestamp}`;
+
+    const { success, signature } = await rnBiometricsInstance.createSignature(
+      {
+        promptMessage: 'Sign in',
+        payload,
+      },
+    );
+
+    if (!success) {
+      Alert.alert("Oops!",
+        "Validation With Face ID fails, try again"
+      )
+    } else {
+      console.log(`Must now checks into database the publicKey linked with the user and validates if signature is valid between storaged into database: ${signature}`)
+    }
+
+  }, [])
+
+  const handleGetFaceID = useCallback(async () => {
+    // Here can be the real logged user
+    const userId = "Jonh Doe"
+    // Here you can get from AsyncStorage one 
+    const storagedUserHasPermission = true
+
+    const rnBiometrics = new ReactNativeBiometrics();
+
+    const { available, biometryType } =
+      await rnBiometrics.isSensorAvailable();
+
+
+    if (!storagedUserHasPermission) {
+      askPermissionForFaceID(available, biometryType!, async () => {
+        // Here you can storage the user choice to nos ask every time for faceID
+
+        // Here is the generated Public key from SSH to storage into database
+        const { publicKey } = await rnBiometrics.createKeys();
+
+        console.log(`Must to link that publicKey to user in database: ${publicKey}`)
+
+        await handleAuthenticateWithFaceID(userId, rnBiometrics)
+      })
+    }
+
+    if (biometryType !== BiometryTypes.FaceID) {
+      Alert.alert(
+        'Oops!',
+        'Face ID is not available on this device.',
+      );
+      return;
+    }
+
+    await handleAuthenticateWithFaceID(userId, rnBiometrics)
+  }, [])
+
   return (
     <SafeArea>
       <FaceIDScreenView>
@@ -23,6 +99,18 @@ export const FaceIDScreen = ({ navigation }: FaceIDScreenProps) => {
             <CaretLeft size={20} weight="bold" color={primaryColor} />
           </HeaderButton>
         </View>
+
+        <TitleContainer>
+          <TitleText>Testing Face ID</TitleText>
+          <SmallText>Click at button bellow to test it.</SmallText>
+        </TitleContainer>
+
+        <SectionView>
+          <FaceIdButton onPress={handleGetFaceID}>
+            <Fingerprint size={24} weight="bold" color="white" />
+            <ButtonText>Try FaceID</ButtonText>
+          </FaceIdButton>
+        </SectionView>
 
       </FaceIDScreenView>
     </SafeArea>
--- src/screens/face-id/style.ts
+++ src/screens/face-id/style.ts
@@ -17,3 +17,47 @@ export const HeaderButton = styled.TouchableOpacity`
   gap: 8px;
 `;
 
+export const TitleContainer = styled.View`
+  align-items: center;
+
+  margin-top: 32px;
+`
+
+export const TitleText = styled.Text`
+  font-weight: bold;
+  color: ${primaryColor};
+
+  text-align: center;
+  font-size: 40px;
+`
+
+export const SmallText = styled.Text`
+  font-size: 16px;
+  font-weight: 200;
+  
+  color: ${primaryColor}
+`
+
+export const SectionView = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
+export const FaceIdButton = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+
+  width: 100%;
+  padding: 16px;
+
+  background-color: ${primaryColor};
+  border-radius: 10px;
+`
+
+export const ButtonText = styled.Text`
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`
